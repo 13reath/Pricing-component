4ccd8160b4e33a127762f057541cfcbaa373cb38
13reath <13reathit@gmail.com>
2025-09-30 19:03:45 +0200

    margin + shadow fix

--- src/layout/BottomLayout.tsx
+++ src/layout/BottomLayout.tsx
@@ -3,15 +3,13 @@ import Button from '../components/Button';
 
 export default function BottomLayout() {
     return (
-        <div className="flex flex-col md:flex-row justify-between items-center max-w-[540px] p-8 md:px-12 gap-8 w-full bg-white rounded-b-xl border-t border-gray-100">
-           
+        <div className="flex flex-col md:flex-row justify-between items-center max-w-[540px] p-8  md:px-12 gap-8 md:mb-20 mb-14 shadow-lg shadow-indigo-500/10 w-full bg-white rounded-b-xl border-t border-gray-100">
             <div className="flex flex-col gap-3">
                 <Checkmark>Unlimited websites</Checkmark>
                 <Checkmark>100% data ownership</Checkmark>
                 <Checkmark>Email reports</Checkmark>
             </div>
 
-           
             <Button>Start my trial</Button>
         </div>
     );
